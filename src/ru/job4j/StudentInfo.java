22f534476751163fadf16a9e5d1409d6fa7fd29f
HankHil <xtorinx@yandex.ru>
2020-03-29 01:50:03 +0300

    start

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("04.10.1991");
+        System.out.println("Kolpakov Anatoliy");
+
+    }
+}
