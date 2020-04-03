2fd6bff1b70c45dfd3d15b3cd5c6fc0998a82a2e
HankHil <xtorinx@yandex.ru>
2020-04-03 04:02:03 +0300

    1

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -17,5 +17,15 @@ public class Converter {
         System.out.println("140 rubles are " + euro + " euro.");
         int dollar = Converter.rubleToDollar(180);
         System.out.println("180 rubles are " + dollar + " dollar.");
+        int in = 140;
+        int expected = 2;
+        int out = rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        int in2 = 240;
+        int expected2 = 4;
+        int out2 = rubleToDollar(in2);
+        boolean passed2 = expected2 == out2;
+        System.out.println("240 rubles are 4. Test result : " + passed2);
     }
 }
