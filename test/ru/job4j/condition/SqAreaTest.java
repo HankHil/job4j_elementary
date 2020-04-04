a9afceae8c68c0d97abc58e4294c7fd555d91d62
HankHil <xtorinx@yandex.ru>
2020-04-04 03:44:37 +0300

    last

--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,6 +8,11 @@ import static org.junit.Assert.*;
 public class SqAreaTest {
 
     @Test
-    public void whenP6K2ThenSquare() {
+    public void whenP6K2Then2Square() {
+        int p = 6;
+        int k = 2;
+        double expected = 2.0;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(out, expected, 0.01);
     }
 }
