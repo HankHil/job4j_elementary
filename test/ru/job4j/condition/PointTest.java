128c393f91306ec40578ee1e44db351c06f62386
HankHil <xtorinx@yandex.ru>
2020-04-04 03:35:57 +0300

    last

---
+++ test/ru/job4j/condition/PointTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void whenPoints0020ThenDistance20() {
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double expected = 2.0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(out, expected, 0.01);
+    }
+}
