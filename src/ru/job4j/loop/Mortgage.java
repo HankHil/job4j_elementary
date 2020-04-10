b01dc8e9ececc31a7b686e2c6b8bbf928a204d3f
HankHil <xtorinx@yandex.ru>
2020-04-11 01:56:11 +0300

    last

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        double credit = amount + (amount * percent / 100);
+        while (credit > 0) {
+            credit = credit - salary;
+            credit = credit + (credit * percent / 100);
+            year++;
+        }
+        return year;
+    }
+}
