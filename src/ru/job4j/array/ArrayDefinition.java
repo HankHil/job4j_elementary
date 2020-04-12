19770f32ff367db799dd32bab52c44415769031d
HankHil <xtorinx@yandex.ru>
2020-04-13 02:58:20 +0300

    last

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Valeriy Utkin";
+        names[1] = "Aleksandr Kuklachev";
+        names[2] = "Anton Gerashenko";
+        names[3] = "Barak Obama";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
+
     }
 }
