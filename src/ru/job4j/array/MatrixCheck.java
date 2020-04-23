fb9a1a6c967534051a589e8ded94a0dfd5fe4a52
HankHil <xtorinx@yandex.ru>
2020-04-23 06:24:23 +0300

    last

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -4,8 +4,7 @@ public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int index = 0; index < board[row].length; index++) {
-            char temp = board[row][0];
-            if (temp != board[row][index]) {
+            if ('X' != board[row][index]) {
                 result = false;
                 break;
             }
@@ -16,8 +15,7 @@ public class MatrixCheck {
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int index = 0; index < board.length; index++) {
-            char temp = board[0][column];
-            if (temp != board[index][column]) {
+            if ('X' != board[index][column]) {
                 result = false;
                 break;
             }
@@ -32,4 +30,18 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 'X') {
+                if (monoHorizontal(board, index) || monoVertical(board, index)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
 }
